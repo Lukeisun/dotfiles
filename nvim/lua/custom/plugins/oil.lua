20d3f97f5c97a69d5f361cc5cc1f2913c2593575
lukeisun <lukeisun57@gmail.com>
2023-12-08 15:38:11 -0500

    little config refactor

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -3,4 +3,8 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup()
+    vim.keymap.set('n', '<leader>t', ':Oil<CR>', { noremap = true, silent = true })
+  end,
 }
