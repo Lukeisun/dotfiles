d33ddb1a98dd440d3d41e6b5d9c0bd823fcb99dd
lukeisun <delacruz@adinlukas.xyz>
2024-06-13 18:30:03 -0500

    updated nvim config to newer kickstart :D

--- config/nvim/lua/custom/plugins/oil.lua
+++ config/nvim/lua/custom/plugins/oil.lua
@@ -2,9 +2,9 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require("oil").setup()
+    require('oil').setup()
     vim.keymap.set('n', '<leader>t', ':Oil<CR>', { noremap = true, silent = true })
   end,
 }
