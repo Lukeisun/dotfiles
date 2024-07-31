9d709429390cd2f57991d80a759164a90b6fd2fe
lukeisun <delacruz@adinlukas.xyz>
2024-07-30 22:20:25 -0500

    i heckin love emoji picking

--- config/nvim/lua/custom/plugins/oil.lua
+++ config/nvim/lua/custom/plugins/oil.lua
@@ -2,7 +2,8 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   -- Optional dependencies
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   config = function()
     require('oil').setup()
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
